afa3aa39c70572805b8b6bd71f6d8b137fefdfd7
Satya Rohith <me@satyarohith.com>
2021-03-18 12:56:46 +0530

    bump

--- server.ts
+++ server.ts
@@ -1,6 +1,14 @@
 import { serve, serveStatic } from "https://deno.land/x/sift@0.1.6/mod.ts";
 
+console.log({ url: import.meta.url });
+
 serve({
-  "/": serveStatic("index.html", { baseUrl: import.meta.url }),
-  "/:filename+": serveStatic(".", { baseUrl: import.meta.url })
+  "/": serveStatic("index.html", {
+    baseUrl: import.meta.url,
+    intervene: (response) => {
+      response.headers.set("Content-Type", "text/html; charset=UTF-8");
+      return response;
+    },
+  }),
+  "/:filename+": serveStatic(".", { baseUrl: import.meta.url }),
 });
