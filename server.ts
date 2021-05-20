fa14670c712d2158174130a1655e1e3513f22476
Satya Rohith <me@satyarohith.com>
2021-05-20 12:36:41 +0530

    test new version

--- server.ts
+++ server.ts
@@ -1,18 +1,16 @@
-import { serve, serveStatic } from "https://deno.land/x/sift@0.1.6/mod.ts";
-
-console.log({ url: import.meta.url });
+import { serve, serveStatic } from "https://deno.land/x/sift@0.3.0/mod.ts";
 
 serve({
   "/": serveStatic("index.html", {
     baseUrl: import.meta.url,
-    intervene: (response) => {
+    intervene: (_req, response) => {
       response.headers.set("content-type", "text/html; charset=utf-8");
       return response;
     },
   }),
   "/:filename+": serveStatic(".", {
     baseUrl: import.meta.url,
-    intervene: (response) => {
+    intervene: (_req, response) => {
       console.info("res url:", response.url);
       console.info("res content-type:", response.headers.get("content-type"));
       return response;
