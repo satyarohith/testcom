966f24097377df74f5c4638b46debbb55908af38
Satya Rohith <me@satyarohith.com>
2021-05-27 09:55:22 +0530

    feat: add /dev route

--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
-import { serve, serveStatic } from "https://deno.land/x/sift@0.3.1/mod.ts";
+import {
+  json,
+  serve,
+  serveStatic,
+} from "https://deno.land/x/sift@0.3.1/mod.ts";
 
 serve({
   "/": serveStatic("index.html", {
     baseUrl: import.meta.url,
   }),
+  "/dev": () => json({ message: "Hello" }),
   "/:filename+": serveStatic(".", {
     baseUrl: import.meta.url,
   }),
